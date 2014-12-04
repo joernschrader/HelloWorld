aa923fb38f119103d5be3a8cfd538454fc413ffe
Jörn Schrader <joern@grillschrat.de>
2014-12-04 07:59:26 +0100

    finally implemented fragment support

--- src/de/wvsg/helloworld/MainActivity.java
+++ src/de/wvsg/helloworld/MainActivity.java
@@ -1,55 +1,47 @@
 package de.wvsg.helloworld;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import de.wvsg.helloworld.HelloWorldFragment.OnLanguageChooserDisplayListener;
-import de.wvsg.helloworld.LanguageChooserFragment.OnLanguageChoosenListener;
 
+public class MainActivity extends ActionBarActivity implements
+		OnLanguageChooserDisplayListener {
 
-public class MainActivity extends ActionBarActivity implements 
-  	OnLanguageChoosenListener, OnLanguageChooserDisplayListener {
-
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);	
-		HelloWorldFragment hwFragment = new HelloWorldFragment();
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		transaction.add(R.id.fragment_container, hwFragment, HelloWorldFragment.DEFAULT_HELLOWORLD_FRAGMENT_TAG);
-		transaction.addToBackStack(null);
-		transaction.commit();
-	}
-	
-	
-	@Override
-	public void onLanguageChoosen(int languageId) {		
-		HelloWorldFragment hwFragment = (HelloWorldFragment) 
-				getSupportFragmentManager().findFragmentByTag(HelloWorldFragment.DEFAULT_HELLOWORLD_FRAGMENT_TAG);
-		if (hwFragment != null) {	
-			FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-			transaction.add(R.id.fragment_container, hwFragment);			
-			transaction.addToBackStack(null);
+		setContentView(R.layout.activity_main);
+		if (findViewById(R.id.fragment_container) != null) {
+			if (savedInstanceState != null) {
+				return;
+			}
+			HelloWorldFragment hwFragment = new HelloWorldFragment();
+			FragmentTransaction transaction = getSupportFragmentManager()
+					.beginTransaction();
+			transaction.add(R.id.fragment_container, hwFragment,
+					HelloWorldFragment.DEFAULT_HELLOWORLD_FRAGMENT_TAG);
 			transaction.commit();
-			hwFragment.updateHelloWorldTag(languageId);
-		} 
+		}
 	}
-
 
 	@Override
 	public void displayLanguageChoose() {
-		LanguageChooserFragment lcFragment = (LanguageChooserFragment) 
-				getSupportFragmentManager().findFragmentByTag(LanguageChooserFragment.DEFAULT_LANGUAGECHOOSE_FRAGMENT_TAG);
-		if (lcFragment == null) {			
-			lcFragment = new LanguageChooserFragment();
+		Intent listIntent = new Intent(this, MainListActivity.class);
+		startActivityForResult(listIntent, 0);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == Activity.RESULT_OK) {
+			int languageId = data.getIntExtra("languageId", 0);
+			HelloWorldFragment hwFragment = (HelloWorldFragment) getSupportFragmentManager()
+					.findFragmentByTag(
+							HelloWorldFragment.DEFAULT_HELLOWORLD_FRAGMENT_TAG);
+			hwFragment.updateHelloWorldTag(languageId);
 		}
-		//LanguageChooserFragment lcFragment = new LanguageChooserFragment();
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		transaction.replace(R.id.fragment_container, lcFragment,LanguageChooserFragment.DEFAULT_LANGUAGECHOOSE_FRAGMENT_TAG);
-		//transaction.addToBackStack(null);
-		transaction.commit();
 	}
 
 }
--- src/de/wvsg/helloworld/MainListActivity.java
+++ src/de/wvsg/helloworld/MainListActivity.java
@@ -1,32 +1,38 @@
 package de.wvsg.helloworld;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import de.wvsg.helloworld.LanguageChooserFragment.OnLanguageChoosenListener;
 
-public class MainListActivity extends ListActivity {
+public class MainListActivity extends ActionBarActivity implements
+		OnLanguageChoosenListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		// fill ListView
-		String[] daten = { "Deutsch", "Englisch", "Französisch", "Spanisch",
-				"Tagalog" };
-		setListAdapter(new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, daten));
+		setContentView(R.layout.activity_main_list);
+		Fragment lcFragment = getSupportFragmentManager().findFragmentByTag(
+				LanguageChooserFragment.DEFAULT_LANGUAGECHOOSE_FRAGMENT_TAG);
+		if (lcFragment == null) {
+			lcFragment = new LanguageChooserFragment();
+			FragmentTransaction transaction = getSupportFragmentManager()
+					.beginTransaction();
+			transaction
+					.add(R.id.list_fragment_container,
+							lcFragment,
+							LanguageChooserFragment.DEFAULT_LANGUAGECHOOSE_FRAGMENT_TAG);
+			transaction.commit();
+		}
 	}
 
 	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		Log.d("Ausgabe", "Ausgewählt wurde: " + id);
+	public void onLanguageChoosen(int languageId) {
 		Intent result = new Intent();
-		result.putExtra("languageId", (int) id);
+		result.putExtra("languageId", languageId);
 		setResult(Activity.RESULT_OK, result);
 		finish();
 	}
